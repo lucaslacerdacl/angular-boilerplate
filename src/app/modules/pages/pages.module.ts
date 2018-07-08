d5de1eeae688322ea9ac5011b8fbeb7eee8b0ec4
Lucas Lacerda <lucas.lacerdabh@gmail.com>
2018-07-08 19:48:29 -0300

    Remove unecessary references and create mapper module for models in components and services

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { ComponentsModule } from '../components/components.module';
-import { CrudsModule } from '../cruds/cruds.module';
+import { MapperModule } from '../../mapper/mapper.module';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    ComponentsModule,
-    CrudsModule
+    ComponentsModule
   ],
   declarations: []
 })
