25719d25e4d88ee081957c8a09a18aeadb7fa7a5
Lucas Lacerda <lucas.lacerdabh@gmail.com>
2018-07-28 16:55:23 -0300

    Configure VSTS Test Analitics

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -22,7 +23,13 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: 'coverage',
+      outputFile: 'TEST-JUNIT.xml',
+      suite: '',
+      useBrowserName: true
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
